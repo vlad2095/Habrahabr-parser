263d16c2847bc914cfcbb45f0972e26f417fa06b
Vlad <vladkrapivin1995@gmail.com>
2017-06-20 14:09:39 +0300

    Better(quite better)

--- database_for_parser.py
+++ database_for_parser.py
@@ -8,19 +8,19 @@ class Postgresdb:
 		
 
 	def connect(self):
-		self.database = db.connect(host="localhost", user="postgres", password="pomidor69", dbname="parser1")
+		self.database = db.connect(host="localhost", user="postgres", password="pomidor69", dbname="parser1") 
 		self.cursor = self.database.cursor()
 
 	def create_table(self, name):
-		self.cursor.execute("CREATE TABLE IF NOT EXISTS {0} (id SERIAL PRIMARY KEY, title VARCHAR(255) NOT NULL);".format(name))
+		self.cursor.execute("CREATE TABLE IF NOT EXISTS {0} (id SERIAL PRIMARY KEY, title VARCHAR(255) NOT NULL, content TEXT NOT NULL);".format(name))
 		self.database.commit()
 
 	def close(self):
 		self.database.close()
 
 
-	def inserti(self, title):
-		self.cursor.execute("INSERT INTO titles (title) VALUES ('{0}')".format(title))
+	def inserti(self, title, content):
+		self.cursor.execute("INSERT INTO dtb (title, content) VALUES ('{0}', '{1}')".format(title, content))
 		self.database.commit()
 
 
--- parser.py
+++ parser.py
@@ -9,12 +9,11 @@ def fix_quotes(string):
     fixed = replace(temp, '"','\"')
     return fixed
 
-class HabrParser:
+class Parser:
         
     def __init__(self, base_url):
         self.base_url = base_url
-        self.whatever = ""
-        
+    
     def get_page(self):
                 
         try:
@@ -24,52 +23,72 @@ class HabrParser:
                         
         if res.status_code < 400:
             return res.content
-    
+
+    def get_html_tree(self, html):
+        return lxml.html.fromstring(html)
+
+    def get_link(self, somedata):
+        return somedata.get('href')
+
     def get_path(self, html_tree, link):
         return html_tree.xpath(link)
 
-    def get_link(self, title):
-        return title.get('href')
+    def get_full_text(self, somelist):
+        articles = []
+        path = './/div[@class="content html_format js-mediator-article"]'
+        for link in somelist:
+            link = Parser(link)
+            page = link.get_page()
+            tree = link.get_html_tree(page)
+            links = link.get_path(tree, path)
+            for one in links:
+                text = one.text_content().encode('utf-8')
+                articles.append(text)
+        return(articles)
 
     def get_info(self, html):
         try:    
-            html_tree = lxml.html.fromstring(html)
-            titles_link = './/div[@class="posts_list"]//h2[@class="post__title"]//a[@class="post__title_link"]'
-            synopsis_link = './/div[@class="posts_list"]//div[@class="content html_format"]'
-            titles = parser.get_path(html_tree, titles_link)
-            synopsises = parser.get_path(html_tree, synopsis_link)
+            tree = parser.get_html_tree(html)
+            titles_path = './/div[@class="posts_list"]//h2[@class="post__title"]//a[@class="post__title_link"]'
+            titles = parser.get_path(tree, titles_path)
         except (IndexError, AttributeError):
             return
         titles_list = []
+        link_list = []
         for one in titles:
+            link = parser.get_link(one)
+            link_list.append(link)
             title = one.text_content().encode('utf-8')
             titles_list.append(title)
 
-        return titles_list
+        return titles_list, link_list
 
 
 
 
 
-    def run(self):
-        while True:
-            page = self.get_page()
-            if page is None:
-                time.sleep(0.5)
-                continue
-            self.get_info(page)
-            time.sleep(0.5)
+    # def run(self):
+    #     while True:
+    #         page = self.get_page()
+    #         if page is None:
+    #             time.sleep(0.5)
+    #             continue
+    #         self.get_info(page)
+    #         time.sleep(0.5)
 
 if __name__ == "__main__":
         db = Postgresdb()
         db.connect()
-        db.create_table("titles")
-        for i in range(1,3,1):
-            parser = HabrParser("https://habrahabr.ru/all/page{0}/".format(i))
+        db.create_table("dtb") 
+        for i in range(1,10,1):
+            print(i)
+            parser = Parser("https://habrahabr.ru/all/page{0}/".format(i))
             page = parser.get_page()
-            titles = parser.get_info(page)
-            for title in titles:
-                title = fix_quotes(title)
-                print(title)
-                db.inserti(title)
+            titles, links = parser.get_info(page)
+            articles = parser.get_full_text(links)
+            for i in range(10):
+                print(i)
+                title = fix_quotes(titles[i])
+                article = fix_quotes(articles[i])
+                db.inserti(title, article)
         db.close()
